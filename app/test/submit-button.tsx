f0a38734cd0f19005283e895b66333497c9f59b8
Kailashkc33 <kc.kailash33@gmail.com>
2024-11-23 12:20:46 +1100

    first commit

---
+++ app/test/submit-button.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { useFormStatus } from 'react-dom'
+
+export function SubmitButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-gray-400"
+    >
+      {pending ? 'Submitting...' : 'Submit'}
+    </button>
+  )
+}
+
